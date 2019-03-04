dbd6a5b8859000e9557aacad697f81b04b7c31d2
Wojciech Matuszewski <wm.matuszewski@wp.pl>
2019-03-04 21:11:27 +0100

    better seperation of concerns regarding the calendar

--- src/components/UserCalendar/CalendarEventWrapper.tsx
+++ src/components/UserCalendar/CalendarEventWrapper.tsx
@@ -1,228 +1,17 @@
 import React from 'react';
+import { EventWrapperProps, Event } from 'react-big-calendar';
+import MonthCalendarEventWrapper from './MonthCalendarEventWrapper';
+import OtherCalendarEventWrapper from './OtherCalendarEventWrapper';
+import { CalendarEvent } from './Events';
 
-import styled from '@emotion/styled';
-import { Badge } from 'antd';
-import { useSpring, animated } from 'react-spring';
-import moment from 'moment';
-import {
-  FlexBoxVerticallyCenteredStyles,
-  NotWrappingTextStyles
-} from '../../shared/styles';
-import CalendarEventPopover from './CalendarEventPopover';
-import { CalendarEvent } from './UserCalendar';
-import classNames from 'classnames';
-import {
-  EventWrapperProps as _EventWrapperProps,
-  Event
-} from 'react-big-calendar';
-import { ColorTint } from './ColorTints';
-
-interface EventWrapperProps<T extends Event = Event>
-  extends _EventWrapperProps<T> {
+export interface CorrectedEventWrapperProps<T extends Event = Event>
+  extends EventWrapperProps<T> {
   continuesPrior: boolean;
   continuesAfter: boolean;
 }
 
-interface EventTileWrapperProps {
-  isDay: boolean;
-  colorTint: ColorTint;
-}
-
-const EventTileWrapper = styled(animated.div, {
-  shouldForwardProp: prop => prop !== 'isDay'
-})`
-  background: ${(props: EventTileWrapperProps) =>
-    props.colorTint.weekTileColor} !important;
-  border-left: 3px solid
-    ${(props: EventTileWrapperProps) => props.colorTint.weekBorderColor} !important;
-  border-right: 0 !important;
-  border-top: 0 !important;
-  border-bottom: 0 !important;
-  border-radius: 0 !important;
-  transition: transform 0.3s ease;
-  box-sizing: border-box;
-  @media screen and (min-width: 800px) {
-    &.clicked {
-      z-index: 99;
-      background: ${(props: EventTileWrapperProps) =>
-        props.colorTint.weekBorderColor} !important;
-      transform: scale(
-        ${({ isDay }: EventTileWrapperProps) => (isDay ? 1 : 1.1)}
-      );
-    }
-  }
-`;
-
-const EventTileWrapperInner = styled.div`
-  width: 100%;
-  height: 100%;
-  span.title {
-    font-weight: bold;
-  }
-`;
-
-const MonthCalendarWrapper = styled.div`
-  ${FlexBoxVerticallyCenteredStyles};
-  padding-left: 10px;
-  overflow: hidden;
-  position: relative;
-  background: ${(props: { longerThanOneDay: boolean; colorTint: ColorTint }) =>
-    props.longerThanOneDay
-      ? `${props.colorTint.spanningMoreThanOneDayColor}`
-      : 'transparent'};
-  .ant-badge-status-processing {
-    background: ${(props: { colorTint: ColorTint }) =>
-      props.colorTint.dotColor};
-  }
-  .ant-badge-status-success {
-    background: ${(props: { colorTint: ColorTint }) =>
-      props.colorTint.dotColor};
-  }
-  .ant-badge-status-processing::after {
-    border: 1px solid
-      ${(props: { colorTint: ColorTint }) => props.colorTint.dotColor};
-  }
-  @media screen and (min-width: 800px) {
-    transition: transform 0.3s ease;
-    &:hover,
-    &.otherPartHovered {
-      cursor: pointer;
-      background: white;
-    }
-    &.clicked {
-      box-shadow: 0 2px 8px rgba(0, 0, 0, 0.15);
-      border-radius: 2px;
-      background: white;
-    }
-  }
-`;
-
-const MonthCalendarInnerWrapper = styled.div`
-  display: flex;
-  flex-direction: row;
-  flex-wrap: nowrap;
-  white-space: nowrap;
-  span {
-    font-weight: bold;
-  }
-`;
-
-const OtherCalendarEventWrapper: React.FC<
-  EventWrapperProps<CalendarEvent> & {
-    parsedStyles: React.CSSProperties;
-    isDay: boolean;
-  }
-> = props => {
-  const animationProps = useSpring({ opacity: 1, from: { opacity: 0 } });
-
-  const [clicked, setClicked] = React.useState<boolean>(false);
-  function toggleClicked() {
-    setClicked(!clicked);
-  }
-
-  return (
-    <EventTileWrapper
-      colorTint={props.event.colorTint}
-      isDay={props.isDay}
-      className={`rbc-event ${clicked ? 'clicked' : ''}`}
-      style={{ ...props.parsedStyles, ...animationProps }}
-    >
-      <CalendarEventPopover onVisibilityChange={toggleClicked}>
-        <EventTileWrapperInner className="calendarPopupContainer">
-          <span className="title">works</span>
-          <p>Title</p>
-        </EventTileWrapperInner>
-      </CalendarEventPopover>
-    </EventTileWrapper>
-  );
-};
-
-const MonthCalendarEventWrapper: React.FC<
-  EventWrapperProps<CalendarEvent>
-> = props => {
-  function getCorrectClasses() {
-    if (props.continuesPrior) {
-      return {
-        lookup: `${props.event.id}-continuesAfter`,
-        self: `${props.event.id}-continuesPrior`
-      };
-    } else if (props.continuesAfter) {
-      return {
-        lookup: `${props.event.id}-continuesPrior`,
-        self: `${props.event.id}-continuesAfter`
-      };
-    } else return null;
-  }
-
-  const spanningIds = getCorrectClasses();
-
-  function checkAndReturnOtherEventPart() {
-    if (!spanningIds) return null;
-    const elem = document.getElementById(spanningIds.lookup);
-    if (!elem) return null;
-    return elem;
-  }
-
-  const start = moment(props.event.start);
-  const end = moment(props.event.end);
-
-  const isLongerThanOneDay = !end.isSame(start, 'day');
-  const [clicked, setClicked] = React.useState<boolean>(false);
-
-  const wrapperClasses = classNames({
-    clicked: clicked
-  });
-
-  function toggleClicked() {
-    if (spanningIds) {
-      const elem = document.getElementById(spanningIds.lookup);
-      if (elem && !clicked) {
-        elem.classList.add('clicked');
-      }
-      if (elem && clicked) {
-        elem.classList.remove('clicked');
-      }
-    }
-    setClicked(!clicked);
-  }
-
-  function handleMouseEnter() {
-    const elem = checkAndReturnOtherEventPart();
-    if (!elem) return;
-    elem.classList.add('otherPartHovered');
-  }
-
-  function handleMouseLeave() {
-    const elem = checkAndReturnOtherEventPart();
-    if (!elem) return;
-    elem.classList.remove('otherPartHovered');
-  }
-
-  return (
-    <CalendarEventPopover onVisibilityChange={toggleClicked}>
-      <MonthCalendarWrapper
-        colorTint={props.event.colorTint}
-        longerThanOneDay={isLongerThanOneDay}
-        className={wrapperClasses}
-        onMouseEnter={handleMouseEnter}
-        onMouseLeave={handleMouseLeave}
-        id={`${spanningIds ? spanningIds.self : ''}`}
-      >
-        <MonthCalendarInnerWrapper>
-          {!props.continuesPrior && (
-            <Badge status={clicked ? 'processing' : 'success'} />
-          )}
-          <span css={[NotWrappingTextStyles]}>
-            {props.event.title} {start.format('h:mma')} - {end.format('h:mma')}
-          </span>
-        </MonthCalendarInnerWrapper>
-      </MonthCalendarWrapper>
-    </CalendarEventPopover>
-  );
-};
-
 class CalendarEventWrapper extends React.Component<
-  EventWrapperProps<CalendarEvent> & { calendarView: string }
+  CorrectedEventWrapperProps<CalendarEvent> & { calendarView: string }
 > {
   private isMonth() {
     return this.props.calendarView === 'month';
@@ -242,7 +31,6 @@ class CalendarEventWrapper extends React.Component<
   render() {
     const parsedStyles = this.parseSuppliedStyles();
     const { calendarView } = this.props;
-
     return this.isMonth() ? (
       <MonthCalendarEventWrapper {...this.props} />
     ) : (
--- src/components/UserCalendar/UserCalendar.tsx
+++ src/components/UserCalendar/UserCalendar.tsx
@@ -6,151 +6,28 @@ import BigCalendar, { View } from 'react-big-calendar';
 import CalendarToolbar from './CalendarToolbar';
 import CalendarEventWrapper from './CalendarEventWrapper';
 import useMedia from '../../hooks/useMedia';
-import { ColorTint, getCalendarColorTint } from './ColorTints';
-
-export interface CalendarEvent {
-  id: number;
-  title: string;
-  allDay?: boolean;
-  start: Date;
-  end: Date;
-  colorTint: ColorTint;
-}
-
-const events: CalendarEvent[] = [
-  {
-    id: 0,
-    title: 'All Day Event very long title',
-    allDay: true,
-    start: new Date(2015, 3, 0),
-    end: new Date(2015, 3, 1),
-    colorTint: getCalendarColorTint()
-  },
-  {
-    id: 1,
-    title: 'Long Event',
-    start: new Date(2015, 3, 7),
-    end: new Date(2015, 3, 10),
-    colorTint: getCalendarColorTint()
-  },
-
-  {
-    id: 2,
-    title: 'DTS STARTS',
-    start: new Date(2016, 2, 13, 0, 0, 0),
-    end: new Date(2016, 2, 20, 0, 0, 0),
-    colorTint: getCalendarColorTint()
-  },
-
-  {
-    id: 3,
-    title: 'DTS ENDS',
-    start: new Date(2016, 10, 6, 0, 0, 0),
-    end: new Date(2016, 10, 13, 0, 0, 0),
-    colorTint: getCalendarColorTint()
-  },
-
-  {
-    id: 4,
-    title: 'Some Event',
-    start: new Date(2015, 3, 9, 0, 0, 0),
-    end: new Date(2015, 3, 10, 0, 0, 0),
-    colorTint: getCalendarColorTint()
-  },
-  {
-    id: 5,
-    title: 'Conference',
-    start: new Date(2015, 3, 11),
-    end: new Date(2015, 3, 13),
-    colorTint: getCalendarColorTint()
-  },
-  {
-    id: 6,
-    title: 'Meeting',
-    start: new Date(2015, 3, 12, 10, 30, 0, 0),
-    end: new Date(2015, 3, 12, 12, 30, 0, 0),
-    colorTint: getCalendarColorTint()
-  },
-  {
-    id: 7,
-    title: 'Lunch',
-    start: new Date(2015, 3, 12, 12, 0, 0, 0),
-    end: new Date(2015, 3, 12, 13, 0, 0, 0),
-    colorTint: getCalendarColorTint()
-  },
-  {
-    id: 8,
-    title: 'Meeting',
-    start: new Date(2015, 3, 12, 14, 0, 0, 0),
-    end: new Date(2015, 3, 12, 15, 0, 0, 0),
-    colorTint: getCalendarColorTint()
-  },
-  {
-    id: 22,
-    title: 'sds',
-    start: new Date(2015, 3, 12, 14, 0, 0, 0),
-    end: new Date(2015, 3, 12, 16, 0, 0, 0),
-    colorTint: getCalendarColorTint()
-  },
-  {
-    id: 22,
-    title: 'ala ma psa',
-    start: new Date(2015, 3, 12, 14, 0, 0, 0),
-    end: new Date(2015, 3, 12, 16, 0, 0, 0),
-    colorTint: getCalendarColorTint()
-  },
-  {
-    id: 9,
-    title: 'Happy Hour',
-    start: new Date(2015, 3, 12, 17, 0, 0, 0),
-    end: new Date(2015, 3, 12, 17, 30, 0, 0),
-    colorTint: getCalendarColorTint()
-  },
-  {
-    id: 10,
-    title: 'Dinner',
-    start: new Date(2015, 3, 12, 20, 0, 0, 0),
-    end: new Date(2015, 3, 12, 21, 0, 0, 0),
-    colorTint: getCalendarColorTint()
-  },
-  {
-    id: 11,
-    title: 'Birthday Party',
-    start: new Date(2015, 3, 13, 7, 0, 0),
-    end: new Date(2015, 3, 13, 10, 30, 0),
-    colorTint: getCalendarColorTint()
-  },
-  {
-    id: 12,
-    title: 'Late Night Event',
-    start: new Date(2015, 3, 17, 19, 30, 0),
-    end: new Date(2015, 3, 18, 2, 0, 0),
-    colorTint: getCalendarColorTint()
-  },
-  {
-    id: 12.5,
-    title: 'Late Same Night Event',
-    start: new Date(2015, 3, 17, 19, 30, 0),
-    end: new Date(2015, 3, 17, 23, 30, 0),
-    colorTint: getCalendarColorTint()
-  },
-  {
-    id: 13,
-    title: 'Multi-day Event',
-    start: new Date(2019, 3, 3, 8, 0, 0),
-    end: new Date(2019, 3, 3, 2, 0, 0),
-    colorTint: getCalendarColorTint()
-  },
-  {
-    id: 14,
-    title: 'Today',
-    start: new Date(new Date().setHours(new Date().getHours() - 3)),
-    end: new Date(new Date().setHours(new Date().getHours() + 3)),
-    colorTint: getCalendarColorTint()
-  }
-];
+import { CalendarEvents } from './Events';
 
 const localizer = BigCalendar.momentLocalizer(moment);
+
+const OverriddenCalendarStyles = css`
+  height: 100vh;
+  .rbc-date-cell a {
+    font-size: 18px;
+  }
+  .rbc-show-more {
+    padding-left: 10px;
+    padding-top: 2px;
+  }
+  .rbc-time-view {
+    overflow: hidden;
+  }
+  .rbc-row-segment {
+    a {
+      background: transparent;
+    }
+  }
+`;
 
 const UserCalendar: React.FC = () => {
   const isOnMobile = useMedia('(max-width: 800px)');
@@ -176,25 +53,14 @@ const UserCalendar: React.FC = () => {
     <BigCalendar
       css={css`
         ${BigCalendarStyles};
-        height: 100vh;
-
-        .rbc-date-cell a {
-          font-size: 18px;
-        }
-        .rbc-show-more {
-          padding-left: 10px;
-          padding-top: 2px;
-        }
-        .rbc-time-view {
-          overflow: hidden;
-        }
+        ${OverriddenCalendarStyles};
         .rbc-time-content {
           padding-right: ${scrollXOffset}px;
           box-sizing: content-box;
         }
       `}
       localizer={localizer}
-      events={events}
+      events={CalendarEvents}
       defaultDate={new Date(2015, 3, 12)}
       onView={setCalendarView}
       view={calendarView}
